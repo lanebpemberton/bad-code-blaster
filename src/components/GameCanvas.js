3e8ed8e43d3351224eea5bebe794ab816713d66e
Lane Pemberton <lanebpemberton@gmail.com>
2021-07-11 01:15:48 -0400

    added classes and components for game engine

--- src/components/GameCanvas.js
+++ src/components/GameCanvas.js
@@ -1,23 +1,22 @@
-import React, { useEffect } from 'react';
+import {Component} from 'react';
+import Backgrounds from '../engine/Backgrounds';
 
 // Here we are importing a CSS file as a dependency
 import '../styles/GameCanvas.css';
 
-//here we setup inline styles as a js object
-// const styles = {
-//     titleBar: {
-//         fontWeight:10
-//     }
-// }
-
-
-
-function GameCanvas()
+class GameCanvas extends Component
 {
-    return (
-        <canvas id="gameCanvas" className="gameCanvas">
-            </canvas>
-    );
+    componentDidMount()
+    {
+        //initialize background of game area
+        let background = new Backgrounds();
+        //draw initial artwork
+        background.drawRedBox();
+    }
+    render()
+    {
+        return (<canvas id="gameCanvas" className="gameCanvas"></canvas>);
+    }
 }
 
 export default GameCanvas;
