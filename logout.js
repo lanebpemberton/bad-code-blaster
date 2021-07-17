eac56353e42187094402480ac7371a9a5c4a02d3
chandler042 <Chandler042@gmail.com>
2021-07-17 15:03:58 -0400

    logout

---
+++ logout.js
@@ -0,0 +1,11 @@
+module.exports = async function(req,res)
+
+{
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+}
