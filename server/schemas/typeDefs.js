6ccd442dc5a617a7d0ce6b7c7c40315fdf940b75
chandler042 <Chandler042@gmail.com>
2021-07-21 20:46:14 -0400

    changed typeDef mutations from Auth to User

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -19,8 +19,8 @@ const typeDefs = gql`
     }
   
     type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): User
+        addUser(username: String!, email: String!, password: String!): User
         
 }
 `;
