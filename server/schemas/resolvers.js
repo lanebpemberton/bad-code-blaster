90f9fc13094e9c4f62bdb12e7839002129bca1ce
chandler042 <Chandler042@gmail.com>
2021-07-21 21:08:47 -0400

    Brent, Ryan, and Chandler: getting datauser into mongodb

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,9 +16,9 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
-            const token = signToken(user);
+            // const token = signToken(user);
 
-            return { token, user };
+            return user;
         },
         login: async (parent, { email, password }) => {
             const user = await User.findOne( { email });
@@ -29,8 +29,8 @@ const resolvers = {
             if(!correctPw) {
                 throw new AuthenticationError('Incorrect credentials')
             }
-            const token = signToken(user);
-            return { token, user };
+            // const token = signToken(user);
+            return user;
         }
     }
 };
