d76e849161a23dd3e4abcf213e904e17abe3f0ce
Brent <brentbaughan1@gmail.com>
2021-07-24 18:25:30 -0400

    made changes to typeDefs and resolvers to accomodate new types, models, etc.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,40 +6,34 @@ const typeDefs = gql`
     type User {
         _id: ID
         username: String
-        email: String
-        
-            
+        email: String    
     }
+
     type Ship {
         ship_name: String
-        speed: Number
-        hull_strength: Number
+        speed: Int
+        hull_strength: Int
         ship_description: String
         sprite: String
-        width: Number
-        height: Number
-
-
-
-
-
+        width: Int
+        height: Int
     }
 
     type Highscore {
         user_id: String
         ship_id: String
-        score: Number
-        time_alive: Number  
-        enemies_killed: Number
-        bad_code_blasted: Number
-        timestamp: Date
-
-
+        score: Int
+        time_alive: Int  
+        enemies_killed: Int
+        bad_code_blasted: Int
+        timestamp: String
     }
+
     type Query {
         me: User
-        getUserHighScore(user_id: ID): [Highscores]
+        getUserHighScore(user_id: ID): [Highscore]
     }
+
     type Auth {
     token: ID!
     user: User
@@ -48,6 +42,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): User
         addUser(username: String!, email: String!, password: String!): User
+        changeShip(user_id: ID, ship_id: ID): User
     }
 `;
 
