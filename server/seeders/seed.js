549dfe07e1bea1179cd2e0fdbf412ea94f34a892
Trenton <markhamtrenton@gmail.com>
2021-07-25 22:25:34 -0400

    added json file for ships refactoring code in seed.js-Ryan,Lane,Brent,Trenton

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,32 +1,23 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bcbdb', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-
-
-const ship1 = [{
-    ship_name: 'Code Blaster',
-    speed: 1,
-    hull_strength: 3,
-    ship_description: 'A basic flyer equipped with simple weaponry and average thrusters.',
-    sprite: '../images/ship1.png', //no actual image yet
-    width: 45, //dimensions used in current frontend-dev branch
-    height: 31,
-}];
-
+const shipList = require('../seeders/ships.json');
 const user1 = [{
     username: 'admin',
     email: 'admin@badcodeblaster.com',
     password: 'Spectre007',
     highscores: [],
 }];
+ 
 
 db.Highscore.deleteMany({}).then((db.User.deleteMany({}))).then((db.Ship.deleteMany({})))
-    .then(() => db.Ship.collection.insertMany(ship1))
+    .then(() => db.Ship.collection.insertMany(shipList))
     .then((data) => {
         console.log(data.result.n + ' ship records inserted.')
     })
