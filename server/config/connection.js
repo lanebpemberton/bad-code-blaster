57356565ed5dc17eb62f9482a7337c4c90229a5e
Trenton <markhamtrenton@gmail.com>
2021-07-25 22:25:34 -0400

    added json file for ships refactoring code in seed.js-Ryan,Lane,Brent,Trenton

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const database = process.env.MONGODB_URI || 'mongodb://localhost/bcbdb'
+const database = process.env.MONGODB_URI || 'mongodb+srv://root:forum007bcb2021@cluster0.c44k2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
 mongoose.connect(database, {
