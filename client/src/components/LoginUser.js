9c42d130a27af74cce1e7d7308a2ff2e9cb1441c
Ryan Simkoff <rsimkoff@gmail.com>
2021-07-27 21:17:39 -0400

    working login scripting

--- client/src/components/LoginUser.js
+++ client/src/components/LoginUser.js
@@ -5,7 +5,98 @@ import React from 'react';
 
 import '../styles/loginuser.css';
 // In Navbar, we can assign a style from an object by using curly braces
+///login data send
 
+
+// login function to change pages to game start
+
+// const homeScreen = 
+
+// //send data to login route
+async function postLoginData(data)
+{
+    let fetchResult = await fetch('/api/users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    return fetchResult;
+}
+
+async function onLogin(event)
+{
+    event.preventDefault();
+    //aggregate data from form
+    let userData = {
+        "email":document.getElementById(`email-login`).value.trim(),
+        "password":document.getElementById(`password-login`).value.trim(),
+    };
+    let loginResults = await postLoginData(userData);
+    console.log(loginResults);
+    //check status
+    if(loginResults.ok)
+    {
+        document.location.replace('/');
+    } else {
+        alert('Login information incorrect. Please try again.')
+        return;
+    }
+}
+document
+//   .querySelector("loginButton")
+  .addEventListener('click', onLogin);
+
+
+
+
+// const loginButton = document.getElementById("loginButton");
+// loginButton.addEventListener("click", postLoginData);
+
+
+
+// //send data to login route
+// async function postLoginData(data)
+// {
+//     let fetchResult = await fetch('/api/login', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(data),
+//     });
+//     return fetchResult;
+// }
+
+// //initiate login fetch
+// async function onLogin(event)
+// {
+//     event.preventDefault();
+//     //aggregate data from form
+//     let userData = {
+//         "email":document.getElementById(`email-login`).value.trim(),
+//         "password":document.getElementById(`password-login`).value.trim(),
+//     };
+//     let loginResults = await postLoginData(userData);
+//     //check status
+//     if(loginResults.ok)
+//     {
+//         document.location.replace('/');
+//     } else {
+//         alert('Login information incorrect. Please try again.')
+//         return;
+//     }
+// }
+// document
+//   .querySelector('.login-form')
+//   .addEventListener('submit', onLogin);
+
+
+
+
+
+/////////////
 
 function hideElem() {
     var cardFront = document.getElementById("cardFront");
@@ -36,30 +127,30 @@ function LoginUser() {
                
                <div class="card-front" id="cardFront">
                    <h2 class="card-text">LOGIN</h2>
-                   <form>
-                       <input type="email" class="input-box"
+                   <form className="login-form">
+                       <input type="email" id="email-login" class="input-box"
                        placeholder="Your Email here" required/>
-                       <input type="password" class="input-box"
+                       <input type="password" id="password-login" class="input-box"
                        placeholder="Your Password here" required/>
-                       <button type="submit" id="loginButton" class="submit-btn card-text">Submit</button> 
+                       <button type="submit" id="loginButton" onClick={onLogin} class="submit-btn card-text">Submit</button> 
                        <input type="checkbox"/><span>Remember Me</span>
                    </form>
-                   <button type="button" id="signupShow" onclick={hideElem} class="btn">Sign Up</button>
+                   <button type="button" id="signupShow" onClick={hideElem} class="btn">Sign Up</button>
                    <a href="">Forgot Password</a>
                </div>
                <div class="card-back" id="cardBack" style={{display:'none'}}>
                 <h2>Sign Up</h2>
                 <form>
-                    <input type="email" id="email-login" class="input-box"
+                    <input type="email" id="signupEmail" class="input-box"
                     placeholder="Your Email here" required/>
-                    <input type="password" id="password-login" class="input-box"
+                    <input type="password" id="signupPassword" class="input-box"
                     placeholder="Your Password here" required/>
                     
                     <input type="checkbox"/><span>Remember Me</span>
                 </form>
                 <button type="button" id="signUpbtn" class="btn card-text">Sign Up</button>
                 <a href="">Forgot Password</a>
-                <button type="submit" id="loginShow" onclick={hideElem} class="submit-btn">Already Have an account</button>
+                <button type="submit" id="loginShow" onClick={hideElem} class="submit-btn">Already Have an account</button>
 
                </div>
               
