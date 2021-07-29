8f9b87e2a1281b986ff38ae1be34763eff097352
Brent B <brentbaughan1@gmail.com>
2021-07-29 16:12:52 -0400

    alien text renders readably and randomly generates

--- client/src/game/Enemy.js
+++ client/src/game/Enemy.js
@@ -1,27 +1,25 @@
-import alien from "../styles/images/alien.png";
 import CollidableEntity from "./CollidableEntity";
-
-const enemyTypes = ['foo', 'bar', 'baz']
+import enemyRelationships from "./EnemyViews";
 
 class Enemy extends CollidableEntity {
     constructor(canvas, ctx)
     {
-        const width = 80;
-        const height = 20;
+        const width = 60;
+        const height = 60;
         const numLanes = Math.floor(canvas.width / width)
         const x = Math.floor(Math.random() * numLanes) * 50; //todo: randomly determine x coord
         const y = 0;
         super(x, y, width, height);
 
-        const type = Math.floor(Math.random() * enemyTypes.length)
-        this.enemyType = enemyTypes[type]
+        const type = Math.floor(Math.random() * enemyRelationships.length)
+        this.enemyType = enemyRelationships[type]
         
         this.ctx = ctx;
         this.canvas = canvas;
         this.damage = 6;
         this.sprite = new Image();
-        this.sprite.src = alien;
-        this.speed = 4;
+        this.sprite.src = this.enemyType;
+        this.speed = 2;
     }
 
     draw()
---
+++ client/src/game/EnemyViews.js
@@ -0,0 +1,15 @@
+import alien0 from "../styles/images/enemies/alien_0.png";
+import alien1 from "../styles/images/enemies/alien_1.png";
+import alien2 from "../styles/images/enemies/alien_2.png";
+import alien3 from "../styles/images/enemies/alien_3.png";
+import alien4 from "../styles/images/enemies/alien_4.png";
+
+const enemyRelationships = [
+    alien0,
+    alien1,
+    alien2,
+    alien3,
+    alien4
+]
+
+export default enemyRelationships;
